a3f4346a4ae1f2daf21d7d2dcdd3cb21c202bfe7
xjj <xjj>
2017-03-27 16:50:04 +0800

    完成遍历省市县三级表的功能。

--- app/src/main/java/com/example/administrator/xjjweather/MainActivity.java
+++ app/src/main/java/com/example/administrator/xjjweather/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.administrator.xjjweather;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
